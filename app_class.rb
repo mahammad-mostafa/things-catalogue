0bf0daa6f24ffb9d1547dd36e1b86132dc7dd1f2
alexansaa <alexan.saa@gmail.com>
2023-10-31 11:51:03 -0500

    handle method

--- app_class.rb
+++ app_class.rb
@@ -7,4 +7,14 @@ class App
     @genres = []
     @authors = []
   end
+
+  def handle_option(option)
+    if option.positive? && option < 7
+      listing_items(option)
+    elsif option > 6 && option < 10
+      adding_item(option)
+    else
+      puts 'Wrong number! Try again'
+    end
+  end
 end
