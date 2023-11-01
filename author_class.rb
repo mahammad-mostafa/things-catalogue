5654fc8db5564e3357ae736c6aa175a56f0a122f
alexansaa <alexan.saa@gmail.com>
2023-11-01 17:42:36 -0500

    last change before merging

--- author_class.rb
+++ author_class.rb
@@ -2,6 +2,7 @@ require 'securerandom'
 
 class Author
   attr_accessor :first_name, last_name
+  attr_reader :id, :items
 
   def initialize(first_name, last_name, id = SecureRandom.uuid)
     @first_name = first_name
