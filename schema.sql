75190f6b028007c3ba7f341d68b7e3b54deac03d
alexansaa <alexan.saa@gmail.com>
2023-11-01 11:51:36 -0500

    game table creation squema actualization

--- schema.sql
+++ schema.sql
@@ -5,4 +5,15 @@ CREATE TABLE Author (
     last_name VARCHAR(255)
 );
 
-
+-- Game Table creation
+CREATE TABLE Game (
+    id INT PRIMARY KEY,
+    genre VARCHAR(255),
+    author VARCHAR(255),
+    source VARCHAR(255),
+    label VARCHAR(255),
+    publish DATE,
+    archived BOOLEAN,
+    multiplayer BOOLEAN,
+    last_played_at DATE
+);
