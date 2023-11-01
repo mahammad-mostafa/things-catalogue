520021d07d90d8d487938b6e26fc988970a9a75d
alexansaa <alexan.saa@gmail.com>
2023-11-01 10:04:11 -0500

    game_class json preserve data imp and input

--- game_class.rb
+++ game_class.rb
@@ -1,4 +1,6 @@
 require_relative 'item_class'
+require_relative 'genre_class'
+
 require 'date'
 
 class Game < Item
@@ -11,7 +13,7 @@ class Game < Item
     @last_played_at = last_played_at
     super.add_genre(genre)
     super.add_author(author)
-    super.label(label)
+    super.add_label(label)
   end
 
   def can_be_archived?
@@ -20,10 +22,22 @@ class Game < Item
   end
 
   def generate_string
-    { multiplayer: @multiplayer, last_played_at: @last_played_at, genre, author, label, publish_date}
+    { multiplayer: @multiplayer, last_played_at: @last_played_at, genre: super.genre.generate_string, author: super.author.generate_string, label: super.label.generate_string, publish_date: super.publish_date.generate_string}
   end
 
-  def parse_string(arguments)
-    new(arguments['multiplayer'], arguments['last_payed_at'])
+  def self.parse_string(arguments)
+    # new(arguments['multiplayer'], arguments['last_payed_at'], Genre.parse_string(arguments['']))
+  end
+
+  def self.input_arguments
+    print 'Is multiplayer?'
+    multiplayer = gets.chomp
+    last_played_at = Date.today
+    # print 'What genre is it?'
+    # gnr = gets.chomp
+    # print 'Who is the author?'
+    # athr = gets.chomp
+    # print ''
+    new(multiplayer,last_played_at)
   end
 end
